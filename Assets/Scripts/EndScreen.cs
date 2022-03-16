9afac18146d5bd2ccc4302219c03f0d7474ada20
Ragim Gaibatov <ragimgaibatov@gmail.com>
2022-03-16 12:51:04 +0300

    End Screen

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI finalScoreText;
+    private ScoreKeeper scoreKeeper;
+    
+    
+    void Start()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Congratulations!\n Your got a score of " + scoreKeeper.CalculateScore() + "%";
+    }
+    
+}
